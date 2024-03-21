f0eab8f347ed2c14e3006e58395f2480a6d6cdca
xabimolana <164369840+xabimolana@users.noreply.github.com>
2024-03-21 22:26:27 +0700

    Update lib.rs

--- programs/solquad/src/lib.rs
+++ programs/solquad/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("5sFUqUTjAMJARrEafMX8f4J1LagdUQ9Y8TR8HwGNHkU8");
+declare_id!("7vJtYPsibZzmnxYMMQ2VKYsGSimEqW92FQ8frvYGJL7H");
 
 #[program]
 pub mod solquad {
